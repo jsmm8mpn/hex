8d95e8c8057eacfa2de254efff19343e51a6cf09
Matthew Nienow <rockopera8@gmail.com>
2014-04-10 23:31:45 -0500

    Got expanding/collapsing of board size completely working.

--- client/js/modules/mapEditor.js
+++ client/js/modules/mapEditor.js
@@ -25,7 +25,7 @@ angular.module('hex.editor', [])
     }
     
     return {
-      generateTiles: function(rows, cols, size, padding) {
+      generateTiles: function(rows, cols, size, padding, startRow, startCol) {
         var size = size;
         var padding = padding;
         var r = size * Math.cos(degreesToRadians(30));
@@ -35,11 +35,13 @@ angular.module('hex.editor', [])
         var height = size + h + d;
         var originX = 50;//- (width * 4);
         var originY = 50;//- (height * 4);
+        startRow = startRow || 0;
+        startCol = startCol || 0;
   
         function calculateHexTile() {
           var vertices = [];
-          for (var row = 0; row < rows; row++) {
-            for (var col = 0; col < cols; col++) {
+          for (var row = startRow; row < rows; row++) {
+            for (var col = startCol; col < cols; col++) {
               var xy = calculateXY(originX, originY, col, row);
               vertices.push({
                 vertice: calculateHexVertices(xy[0], xy[1])
@@ -67,6 +69,9 @@ angular.module('hex.editor', [])
             var rowsToRemove = rows - newRows;
             var startIndex = tiles.length - cols * rowsToRemove;
             newTiles = tiles.slice(0,startIndex);
+          } else if (newRows > rows) {
+            var rowsToAdd = newRows - rows;
+            newTiles = newTiles.concat(this.generateTiles(newRows, cols, 20, 3, rows));
           }
         }
         if (newCols) {
@@ -78,6 +83,12 @@ angular.module('hex.editor', [])
               }
             }
             newTiles = left;
+          } else if (newCols > cols) {
+            for (var row = 0; row < (newRows || rows); row++) {
+              var toAdd = this.generateTiles(row+1, newCols, 20, 3, row, cols);
+              var args = [row * newCols + cols, 0].concat(toAdd);
+              Array.prototype.splice.apply(newTiles, args);
+            }
           }
         }
         return newTiles;
